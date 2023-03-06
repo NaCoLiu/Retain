f7c762eea5ac1debf9b658ec65acef799fa2b38e
NaCo <naco@nco.im>
2023-03-07 07:41:33 +0800

    feature(text): changeCopyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default defineComponent({
   setup() {
     return () => (
       <div class="p-10 text-gray-400 text-sm text-center" style="letter-spacing: -1px;">
-        Copyright &copy; 1998 - 2023 NaCoLiu. All Rights Reserved. <br/>Power By Nuxt & Typecho create.
+        Copyright &copy; 2016 - 2023 NaCoLiu. All Rights Reserved. <br/>Power By Nuxt & Typecho create.
       </div>
     );
   },
